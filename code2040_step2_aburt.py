4d89dc1fce46b6cc00eb6ea73379cf4595728353
daweed <aburthinds@daweedmacpro.home>
2016-08-21 18:25:40 -0400

    Solution to CODE2040 Step Two API challenge

---
+++ code2040_step2_aburt.py
@@ -0,0 +1,21 @@
+import requests
+
+# API access token
+myToken = '3624f3214a21f60da7488a0716e6eee3'
+
+# Code2040 API POST request for challenge info
+step2 = requests.post('http://challenge.code2040.org/api/reverse', data={"token": myToken})
+step2RequestResponse = step2.text
+print(step2RequestResponse)
+
+# Reverses API string using extended slices
+step2RequestResponseReversed = step2RequestResponse[::-1]
+print(step2RequestResponseReversed)
+# Dictionary Created to send with POST request validation
+step2dataToPost = {
+    'string': step2RequestResponseReversed,
+    'token': myToken
+}
+# Sends API POST request for challenge validation and displays text response
+step2Validate = requests.post('http://challenge.code2040.org/api/reverse/validate', data=step2dataToPost)
+print(step2Validate.text)
