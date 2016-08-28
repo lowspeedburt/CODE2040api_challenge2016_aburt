bf87f33b5b7d93f9f1373775f30b7c6cefb703ad
Albert Burt <skinnyal@users.noreply.github.com>
2016-08-28 18:11:21 -0400

    update comments/variable names for clarity

--- code2040_step5_aburt.py
+++ code2040_step5_aburt.py
@@ -6,41 +6,41 @@ from _datetime import datetime
 from datetime import timedelta
 
 
-# API Token
-myToken = '3624f3214a21f60da7488a0716e6eee3'
+# API Token.
+myAPIToken = '3624f3214a21f60da7488a0716e6eee3'
 
-# POST request
-step5 = requests.post('http://challenge.code2040.org/api/dating', data={'token': myToken} )
+# POST request.
+step5 = requests.post('http://challenge.code2040.org/api/dating', data={'token': myAPIToken} )
 
-# Converts POST request to JSON and displays response
+# Converts POST request to JSON and displays Response.
 step5JSONResponse = json.loads(step5.text)
 print(step5JSONResponse)
 
-# Isolates interval and datestamp keys and displays them
+# Isolates Interval and Datestamp Keys and displays them.
 interval = step5JSONResponse['interval']
 datestamp = step5JSONResponse['datestamp']
 print(interval)
 print(datestamp)
 print()
 
-# Converts datestamp string to datetime object and diplays object
+# Converts Datestamp string to Datetime Object and diplays Object.
 dateStampObject = datetime.strptime(datestamp, '%Y-%m-%dT%XZ')
 print(dateStampObject)
 
-# Adds given interval to datestamp object
+# Adds given Interval to Datestamp Object.
 finalDateObject = dateStampObject + timedelta(seconds=interval)
 
-# Converts Final Date object to a String
+# Converts Final Date Object to a String.
 finalDateString = finalDateObject.strftime('%Y-%m-%dT%XZ')
 print(type(finalDateString)) # test that conversion was done correctly
 
-# Dictionary to POST
+# Dictionary to POST.
 dataToPost = {
-    'token' : myToken,
+    'token' : myAPIToken,
     'datestamp' : finalDateString
 }
 
-# API challenge validation POST request, headers and response
+# API challenge Validation POST Request, Headers and Response.
 step5Validate = requests.post('http://challenge.code2040.org/api/dating/validate', json=dataToPost)
 print(step5Validate.request.headers)
 print(step5Validate.text)
