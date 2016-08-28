1b590c5c7e2342b30064b32d972e86a101888b30
Albert Burt <skinnyal@users.noreply.github.com>
2016-08-28 18:06:56 -0400

    update comments/variable names for clarity

--- code2040_step4_aburt.py
+++ code2040_step4_aburt.py
@@ -3,36 +3,36 @@ import requests
 import json
 
 # My API Token
-myToken = '3624f3214a21f60da7488a0716e6eee3'
+myAPIToken = '3624f3214a21f60da7488a0716e6eee3'
 
-# API POST request and response
-step4 = requests.post('http://challenge.code2040.org/api/prefix', data={'token': myToken})
+# API POST Request and Response
+step4 = requests.post('http://challenge.code2040.org/api/prefix', data={'token': myAPIToken})
 print(step4.text)
 
-# JSON decoder
+# Converts POST Request to JSON.
 step4jsonResponse = json.loads(step4.text)
 
-# isolates prefix and array keys and displays prefix
+# Isolates Prefix and Array Keys and displays Prefix
 queryPrefix = step4jsonResponse["prefix"]
 arrayToSearch = step4jsonResponse["array"]
 print(queryPrefix)
 print()
 
-# array to POST
+# Array Collection that will hold all strings that do not contain the target Prefix.
 notTheSamePrefixArray = []
 
-# Searches through list and determines whether target query exists in list
+# Searches through Array Collection and determines whether Target Prefix exists.
 for index in range(len(arrayToSearch)):
     if queryPrefix not in arrayToSearch[index]:
         notTheSamePrefixArray.append(arrayToSearch[index])  # adds strings that do not contain prefix to list
 
 # Dictionary to POST
 dataToPost = {
-    'token': myToken,
+    'token': myAPIToken,
     'array': notTheSamePrefixArray
 }
 
-# API Validation POST request, Request Headers, and response
+# API Validation POST Request, Request Headers, and Response
 step4Validate = requests.post("http://challenge.code2040.org/api/prefix/validate", json=dataToPost)
 print(step4Validate.request.headers)
 print(step4Validate.text)
