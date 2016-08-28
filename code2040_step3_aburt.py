5ab121f882a9e47f8189f2fe061fe1a7f9373963
Albert Burt <skinnyal@users.noreply.github.com>
2016-08-28 18:01:56 -0400

    update comments for clarity

--- code2040_step3_aburt.py
+++ code2040_step3_aburt.py
@@ -5,26 +5,26 @@ import json
 # CODE2040 API Token.
 myToken = '3624f3214a21f60da7488a0716e6eee3'
 
-# POST request for challenge dictionary and response.
+# POST Request for challenge dictionary and Response.
 step3 = requests.post('http://challenge.code2040.org/api/haystack', data={'token': myToken})
 print(step3.text)
 
-# JSON module text to JSON decoding.
+# # Converts POST Request to JSON.
 step3jsonResponse = json.loads(step3.text)
 
-# isolation of target String from API dictionary.
+# Isolation of target String from API dictionary.
 queryNeedle = step3jsonResponse["needle"]
 print(queryNeedle)
 
-# Returns index of the target String found in list.
+# Returns index of the target String found in List.
 foundNeedle = step3jsonResponse["haystack"].index(queryNeedle)
 print(foundNeedle)
 
-# Dictionary to POST for validation.
+# Dictionary to POST for Validation.
 dataToPost = {
     'token': myToken,
     'needle': foundNeedle
 }
-# API Challenge Validation and response.
+# API Challenge Validation and Response.
 step3Validate = requests.post('http://challenge.code2040.org/api/haystack/validate', json=dataToPost)
 print(step3Validate.text)
