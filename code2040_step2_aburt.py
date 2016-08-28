bbdfa8c6cf22848cde63ed6ef2aa65e15dd61e38
Albert Burt <skinnyal@users.noreply.github.com>
2016-08-28 18:07:59 -0400

    update comments/variable names for clarity

--- code2040_step2_aburt.py
+++ code2040_step2_aburt.py
@@ -1,10 +1,10 @@
 import requests
 
 # API access token
-myToken = '3624f3214a21f60da7488a0716e6eee3'
+myAPIToken = '3624f3214a21f60da7488a0716e6eee3'
 
 # Code2040 API POST request for challenge info
-step2 = requests.post('http://challenge.code2040.org/api/reverse', data={"token": myToken})
+step2 = requests.post('http://challenge.code2040.org/api/reverse', data={"token": myAPIToken})
 step2RequestResponse = step2.text
 print(step2RequestResponse)
 
@@ -14,7 +14,7 @@ print(step2RequestResponseReversed)
 # Dictionary Created to send with POST request validation
 step2dataToPost = {
     'string': step2RequestResponseReversed,
-    'token': myToken
+    'token': myAPIToken
 }
 # Sends API POST request for challenge validation and displays text response
 step2Validate = requests.post('http://challenge.code2040.org/api/reverse/validate', json=step2dataToPost)
